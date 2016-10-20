da10eb7c6d6fcbad69e0aba82a4f759431d0aa42
Jason Lantz <jlantz@salesforce.com>
2016-10-20 17:14:58 -0500

    just print content

--- triggers/handlers.py
+++ triggers/handlers.py
@@ -55,4 +55,4 @@ def trigger_circle_build(sender, **kwargs):
     }
 
     response = requests.post(api_url, data)
-    print '[{}] {}'.format(response.status, response.content)
+    print response.content
