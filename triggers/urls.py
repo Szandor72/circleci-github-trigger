154859843495bfb3c0462fd7c22a94a77cd3794e
Jason Lantz <jlantz@salesforce.com>
2016-10-20 15:08:25 -0500

    Fix webhook url

--- triggers/urls.py
+++ triggers/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from triggers import views as triggers_views
 
 urlpatterns = [
-    url(r'^webhook/github/push$', triggers_views.github_push_webhook),
+    url(r'^webhooks/github/push$', triggers_views.github_push_webhook),
 ]
